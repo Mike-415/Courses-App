a4f11d18ab3a7f2c2bfba253d07a4ac72d67a512
Mike-415 <mgonz160@mail.ccsf.edu>
2023-03-05 23:13:17 -0800

    Added card

--- app/src/main/java/com/example/gridapp/MainActivity.kt
+++ app/src/main/java/com/example/gridapp/MainActivity.kt
@@ -1,15 +1,26 @@
-package com.example.gridapp
+@file:Suppress("PreviewAnnotationInFunctionWithParameters")
 
+package com.example.gridapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.gridapp.data.Topic
+import com.example.gridapp.model.DataSource
 import com.example.gridapp.ui.theme.GridAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +33,70 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    GridApp()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    GridAppTheme {
-        Greeting("Android")
+fun GridApp() {
+    CourseCard(DataSource.topics[12])
+}
+
+
+@Composable
+fun CourseCard(topic: Topic){
+    Card(
+        modifier = Modifier
+            .padding(8.dp),
+        elevation = 4.dp
+    ) {
+        Row{
+            Column() {
+                Image(
+                    painter = painterResource(id = topic.topicImageResource),
+                    contentDescription = stringResource(id = topic.topicNameResource),
+                    modifier = Modifier
+                        .width(68.dp)
+                        .height(68.dp),
+                    contentScale = ContentScale.FillBounds
+                )
+            }
+            Column(
+                modifier = Modifier
+                    .align(Alignment.Bottom)
+                    .padding(
+                        start = 16.dp,
+                        top = 16.dp,
+                        end = 16.dp,
+                        bottom = 8.dp
+                    )
+            ){
+                Text(text = stringResource(id = topic.topicNameResource),
+                    fontSize = 12.sp
+                )
+                Row(
+                    modifier = Modifier
+                        .padding(
+                            top = 8.dp
+                        )
+                        .wrapContentSize(unbounded = true),
+                    horizontalArrangement = Arrangement.Start,
+                    verticalAlignment = Alignment.CenterVertically
+                ){
+                    Image(painter = painterResource(id = R.drawable.ic_grain), contentDescription = null)
+                    Spacer(modifier = Modifier.padding(horizontal = 8.dp))
+                    Text(
+                        text = topic.numberOfCourses.toString(),
+                        fontSize = 12.sp
+                    )
+
+                }
+            }
+
+        }
     }
 }
